46ca96be5592b32a8199241ba75ceda8a4832cfc
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-11 12:45:44 +0000

    task5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ type annotations tasks """
+from typing import List
 
 
-def sum_list(input_list: [float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ return sum of list of floats """
     result = 0
     for i in input_list:
